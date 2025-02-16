b768df6fff8bb2be84ea5dd5795b1e57482a4161
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-02-16 18:14:57 +0100

    Implement simple manual gesture example

--- App.tsx
+++ App.tsx
@@ -1,66 +1,51 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
 import {Dimensions, ScrollView, StyleSheet, View} from 'react-native';
 import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedStyle,
+import {
+  cancelAnimation,
   useSharedValue,
-  withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 
 export default function App() {
-  const startPosition = useSharedValue({x: 0, y: 0});
-  const position = useSharedValue({x: 0, y: 0});
-  const scale = useSharedValue(1);
+  const dummy = useSharedValue(0);
 
-  const panGesture = Gesture.Pan()
-    .onBegin(() => console.log('onBegin'))
-    .onStart(() => console.log('onStart'))
-    .onTouchesDown(() => {
-      scale.value = withSpring(1.2);
-      console.log('onTouchesDown');
-    })
-    .onTouchesMove(() => console.log('onTouchesMove'))
-    .onUpdate(({translationX, translationY}) => {
-      console.log('onUpdate');
-      position.value = {
-        x: startPosition.value.x + translationX,
-        y: startPosition.value.y + translationY,
-      };
-    })
-    .onTouchesCancelled(() => console.log('onTouchesCancelled'))
-    .onTouchesUp(() => console.log('onTouchesUp'))
-    .onEnd(() => {
-      startPosition.value = position.value;
-      scale.value = withSpring(1);
-      console.log('onEnd');
-    })
-    .onFinalize(() => console.log('onFinalize'));
+  const gesture = Gesture.Manual()
+    .onTouchesDown((_, manager) => {
+      console.log('onTouchesDown', Date.now());
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateX: position.value.x,
-      },
-      {
-        translateY: position.value.y,
-      },
-      {
-        scale: scale.value,
-      },
-    ],
-  }));
+      dummy.value = withTiming(Math.random(), {duration: 1000}, finished => {
+        if (finished) {
+          manager.activate();
+        }
+      });
+    })
+    .onTouchesUp((_, manager) => {
+      console.log('onTouchesUp', Date.now());
+      manager.fail();
+    })
+    .onTouchesCancelled(() => console.log('onTouchesCancelled', Date.now()))
+    .onFinalize(() => {
+      console.log('onFinalize', Date.now());
+      cancelAnimation(dummy);
+    })
+    .manualActivation(true);
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <ScrollView contentContainerStyle={styles.content}>
-        <Animated.View style={animatedStyle}>
-          <GestureDetector gesture={panGesture}>
-            <View style={styles.box} />
-          </GestureDetector>
-        </Animated.View>
+        <GestureDetector gesture={gesture}>
+          <View style={[styles.box, {backgroundColor: 'red'}]} />
+        </GestureDetector>
+
+        <GestureDetector gesture={gesture}>
+          <View style={[styles.box, {backgroundColor: 'blue'}]} />
+        </GestureDetector>
       </ScrollView>
     </GestureHandlerRootView>
   );
@@ -71,14 +56,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   box: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
+    width: 200,
+    height: 200,
     borderRadius: 10,
   },
   content: {
-    minHeight: Dimensions.get('window').height + 10,
+    minHeight: 2 * Dimensions.get('window').height,
     justifyContent: 'center',
+    gap: 50,
     alignItems: 'center',
   },
 });
